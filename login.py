4ab7c782c57e9443488bcdcd21e743d71307f867
WangZhen <wangzhenjjcn@gmail.com>
2019-01-20 08:29:33 +0800

    add

--- login.py
+++ login.py
@@ -106,12 +106,21 @@ def openSearchPage():
     return responseRes.text
   
 
-def getLinkBySearchPage(pageData):
+def getLinkBySearchPage(pageData,line):
+    line1=""
+    line2="线路"
+    if line==1:
+        line1="<div class='b_line'></div>"
+        line2=line2+str(line)
+    else:
+        line1=line2+str(line-1)
+        line2=line2+str(line)
     print("getLinkBySearchPage:")
     proxyLink=pageData[pageData.index("<div class='b_line'></div>"):] 
     proxyLink=proxyLink[:proxyLink.index("</ul>")]    
-    proxyLink=proxyLink[proxyLink.index("线路3"):proxyLink.index("线路4")] 
-    proxyLink=proxyLink[proxyLink.index("</li><li><a href=\"")+18:proxyLink.index("target=")]  
+    proxyLink=proxyLink[proxyLink.index(line1):proxyLink.index(line2)] 
+    print(proxyLink)
+    proxyLink=proxyLink[proxyLink.index("<li><a href=\"")+13:proxyLink.index("target=")]  
     proxyLink=proxyLink[:proxyLink.index("\"")]                     
     print(proxyLink)
     return proxyLink
@@ -225,7 +234,13 @@ def getLoginAuthPageImageAddrByAuthCode(authCode):
     loginAuthPageImageUrl= "/getVcode/.auth?t=" + t[0] + "&systemversion=" + "4_6_sp9" + "&.auth"
     print(loginAuthPageImageUrl)
     return loginAuthPageImageUrl
-    
+
+def getVerifyValueByAuthCode(authCode):
+    print("getVerifyValueByAuthCode:"+authCode) 
+    t = authCode.split("_")
+    verifyValue= t[1]  
+    print(verifyValue)
+    return verifyValue
     
 def getLoginAuthPageTimeByAuthCode(authCode):  
     print("getLoginAuthPageTimeByAuthCode:"+authCode) 
@@ -242,23 +257,18 @@ def getLoginAuthVerifyValueByAuthCode(authCode):
     return current_page_time
 
 
-def getVerifyCodeByLoginAuthPage(loginAuthPage):
-    print("getVerifyCodeByLoginAuthPage:"+loginAuthPage) 
-    return ""
- 
-def getVerifyValueByLoginAuthPage(loginAuthPage):
-    print("getVerifyValueByLoginAuthPage:"+loginAuthPage) 
-    return ""
-
-
-def getSECByLoginAuthPage(loginAuthPage):
-    print("getVerifyValueByLoginAuthPage:"+loginAuthPage) 
-    return ""
- 
-
-
-
-
+def getValueByLoginAuthPage(loginAuthPage,value):
+    data=""
+    if value in loginAuthPage:
+        data=loginAuthPage[loginAuthPage.index("name=\""+value):]
+        data=data[data.index("value=\"")+7:]
+        data=data[:data.index("\">")]
+        print(data)
+        return data
+    else:
+        return data
+    print("getLoginAuthPageData:"+value+"   is   "+ data) 
+    return data
 
 
 def encodePassword(password):
@@ -282,101 +292,25 @@ def postLogin(proxyLink,username,pwd):
     proxyLinkURI="http://"+proxyLinkURI[:proxyLinkURI.index("/")]
     print("proxyLinkURI:"+proxyLinkURI) 
     loginAuthPage=getLoginAuthPage(proxyLink)
-    verifyCode=getVerifyCodeByLoginAuthPage(loginAuthPage)
-    verifyValue=getVerifyValueByLoginAuthPage(loginAuthPage)
-    sec=getSECByLoginAuthPage(loginAuthPage)
+    
+    
     authCode=getLoginAuthPageImageAuthCode(proxyLinkURI)
     imageAddr=proxyLinkURI+getLoginAuthPageImageAddrByAuthCode(authCode)
     downloadLoginAuthPageImage(imageAddr,"./auth.jpg")
     imageCode=fixNumResault(decodeImage("auth.jpg","./"))
+
+
+
+    verifyCode =imageCode
+    verifyValue =getVerifyValueByAuthCode(authCode)
+    cid =getValueByLoginAuthPage(loginAuthPage,"cid")
+    cname =getValueByLoginAuthPage(loginAuthPage,"cname")
+    systemversion = "4_6_sp9"
     password=encodePassword(pwd)
-    #  <form id="login_form" action="http://54.64.37.20:5201/loginVerify/.auth" style="display: none" method="post" target="_blank">
-    #     <input type="hidden" name="VerifyCode" value="1111">
-    #     <input type="hidden" name="__name" value="1111" id="user_name">
-    #     <input type="hidden" name="__VerifyValue" value="1">
-    #     <input type="hidden" name="systemversion" value="1_0">
-    #     <input type="hidden" name="banbeng" value="1">
-    #     <input type="hidden" name="cname" value="tc">
-    #     <input type="hidden" name="password" value="000000">
-    #     <input type="hidden" name="cid" value="65">
-    #     <input type="hidden" name="loginType" value="1">
-    # </form>
-    # function loginForm (argument) {
-    #         var userName = document.getElementById("user_name"),tN;
-    #         while( !tN || tN > 900){
-    #             tN = tN = parseInt((+(new Date())-(Math.random())*10).toFixed(3).slice(-3).replace('.',''),10);
-    #         }
-    #         userName.value = 't' + parseInt(tN,10);
-    #         document.getElementById("login_form").submit();
-    #     }
 
+ 
 
-    # function() { //使用POST方法發送表單數據
-    #         if (current_page.login_flag == false) {
-    #             return false;
-    #         }
-    #         current_page.login_flag = false;
-    #         var form = document.getElementsByTagName('form')[0];
-    #         var postStr = "";
-    #         postStr += "VerifyCode=" + form.VerifyCode.value + '&';
-    #         postStr += "__VerifyValue=" + form.__VerifyValue.value + '&';
-    #         postStr += "__name=" + form.__name.value + '&';
-    #         var password_txt = document.getElementById('password_txt');
-    #         if(password_txt.style.display != 'none'){
-    #             //Becky 20180525 replace password
-    #             var symbolMappingList = {'`' : '!V$', '-' : '!m$', '=' : '!k$', '[' : '!O$', ']' : '!K$', ';' : '!I$', '\'' : '!S$', '\\' : '!T$', '/' : '!r$', '.' : '!Z$', ',' : '!a$', '~' : '!i$', '!' : '!p$', '@' : '!f$', '#' : '!7$', '$' : '!D$', '%' : '!l$', '^' : '!9$', '&' : '!q$', '*' : '!t$', '(' : '!6$', ')' : '!g$', '_' : '!v$', '+' : '!J$', '{' : '!L$', '}' : '!d$', '|' : '!W$', '"' : '!E$', ':' : '!0$', '?' : '!H$', '>' : '!y$', '<' : '!b$' };
-    #             var original_pwd = form.password.value.split('');
-    #             var replace_pwd  = '';
-    #             for (var property1 in original_pwd) {
-    #                 if(symbolMappingList[original_pwd[property1]] != undefined) {
-    #                    replace_pwd += symbolMappingList[original_pwd[property1]];
-    #                 } else {
-    #                    replace_pwd += original_pwd[property1];
-    #                 }
-    #             }
-
-    #             postStr += "password=" + replace_pwd  + '&';
-    #             //postStr += "password=" + form.password.value + '&';
-    #         }else{
-    #             var objx = document.getElementById("textActive");
-    #             postStr += "password=" + objx.Pwd + '&';
-    #         }
-    #         var sec = document.getElementById('sec');
-    #         postStr += "isSec=" + (sec.checked ? 1 : 0) + '&';
-    #         postStr += "cid=" + form.cid.value + '&';
-    #         postStr += "cname=" + form.cname.value + '&';
-    #         postStr += "systemversion=" + systemversion + '&';
-    #         var ajax = new current_page.Ajax();
-    #         ajax.post("/loginVerify/.auth", postStr, function(xmlhttp) {
-    #             if (xmlhttp == null) {
-    #                 return false;
-    #             }
-    #             if (xmlhttp.responseText.indexOf("êêê") != -1) {
-    #                 var arr = xmlhttp.responseText.split("êêê");
-    #                 document.getElementsById('bg').innerHTML = new Function(arr[1])();
-    #             }
-    #             if (xmlhttp.responseText.indexOf("密碼不正確") != -1) {
-    #                 form.__name.value = '';
-    #                 form.password.value = '';
-    #                 form.__name.focus();
-    #                 var objx = document.getElementById("textActive"),ua = navigator.userAgent.toLowerCase();
-    #                 var isIE = ua.indexOf("trident") > -1 || ua.indexOf("msie") > -1;
-    #                 if(objx && typeof objx.PwdClear == "function" && isIE){
-    #                     objx.PwdClear();
-    #                 }
-    #             }
-    #             if (xmlhttp.responseText.indexOf("://host") == -1) {
-    #                 current_page.login_flag = true;
-    #             }
-    #             xmlhttp.responseText.indexOf("://host") != -1 ? document.location.href = xmlhttp.responseText.split('://host')[1] : alert(xmlhttp.responseText);
-    #             if (xmlhttp.responseText.indexOf("驗證碼已過期") != -1) {
-    #                 form.VerifyCode.value = '';
-    #                 current_page.s(false);
-    #             }
-
-    #         });
-
-    loginURL = "http://pc5.g.nmnmnn.ninja/loginVerify/.auth"
+    loginURL = proxyLinkURI.replace("http","https")+"/loginVerify/.auth"
     loginURLheaders = {
         'origin': "http://pc5.g.nmnmnn.ninja",
         'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
@@ -394,58 +328,31 @@ def postLogin(proxyLink,username,pwd):
     postStr += "__VerifyValue=" + verifyValue + '&'
     postStr += "__name=" + username + '&'
     postStr += "password=" + password  + '&'
+    postStr += "isSec=0" + '&'
+    postStr += "cid=" + cid + '&'
+    postStr += "cname=" + cname + '&'
+    postStr += "systemversion=" + systemversion + '&'
 
-
-
-    
     responseRes = mafengwoSession.post(loginURL, data = postStr, headers = loginURLheaders)
     mafengwoSession.cookies.save()
     print(f"statusCode = {responseRes.status_code}")
     print(f"text = {responseRes.text}")
     # 验证码已过期,请重新刷新。
     return responseRes.text
+   
+   
     
-
-
-# def openProxyPage(proxyLink):
-#     print("openProxyPage:"+proxyLink) 
-#     proxyLinkURI=proxyLink[proxyLink.index("//")+2:]
-#     proxyLinkURI="http://"+proxyLinkURI[:proxyLinkURI.index("/")]
-#     loginAuthPage=getLoginAuthPage(proxyLinkURI)
-#     authCode=getLoginAuthPageImageAuthCode(proxyLinkURI)
-#     imageAddr=proxyLinkURI+getLoginAuthPageImageAddrByAuthCode(authCode)
-#     downloadLoginAuthPageImage(imageAddr,"./auth.jpg")
-#     imageCode=fixNumResault(decodeImage("auth.jpg","./"))
-#     print(imageCode)
-#     return loginAuthPage
-
-
-# VerifyCode=46123
-# &__VerifyValue=e5698f2685ed8b3152e68ae04e52d53357b4Mzc2OTo1OTk5Mn5efn5efnY0NjEyMw
-# &__name= 
-# &password= !Z$
-# &isSec=0
-# &cid=1050033
-# &cname=联通
-# &systemversion=4_6_sp9&
-
-
-
-
-
-    
-    
-def doLogin(account, password):
+def doLogin(account, password,line):
     print("doLogin:"+account+"  "+password) 
     mainPage=openMainPage()
     secPage=openSecPage()
     searchPage=openSearchPage()
-    proxyLink=getLinkBySearchPage(searchPage)
+    proxyLink=getLinkBySearchPage(searchPage,line)
     proxyPage=postLogin(proxyLink,account,password)
-
+    print(proxyPage)
 
     
 
 if __name__ == "__main__":
-    doLogin(" ", " ")
+    doLogin(" ", " ",4)
 
